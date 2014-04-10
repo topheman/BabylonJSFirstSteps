51bffb63f725affaa60c99c92114093d791321bb
Christophe Rosset <tophe@topheman.com>
2014-04-10 15:21:08 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    myAppOptions:{
+      app : 'src',
+      dist : 'dist'
+    },
+    open: {
+      server: {
+        url: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+    connect: {
+      options: {
+        port: 9002,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        keepalive: true
+      },
+      test: {
+        options: {
+          base: '<%= myAppOptions.app %>'
+        }
+      },
+      dist: {
+        options: {
+          base: '<%= yeoman.dist %>'
+        }
+      }
+    }
+  });
+  
+  grunt.registerTask('serve', function(target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist']);
+    }
+
+    grunt.task.run([
+      'open:server',
+      'connect:test'
+    ]);
+  });
+  
+  grunt.registerTask('build', function() {
+    grunt.log.error('build task not yet implemented');
+  });
+
+};
---
+++ README.md
@@ -0,0 +1,11 @@
+BabylonJS first steps
+=====================
+
+Those are my first steps on BabylonJS.
+
+After you have cloned the repository :
+
+* `npm install`
+* `grunt serve` will launch an http server for the `src` folder
+
+Those steps are not mandatory, but it can be comfortable to have an http server.
